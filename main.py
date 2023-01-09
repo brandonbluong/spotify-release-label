a5151dbc7901f198b17b357478642003b3b0790c
Brandon Luong <brandonbluong@gmail.com>
2023-01-09 00:59:19 -0800

    feat: add spotify api endpoint connections to Brandon Luong's user profile (#6)
    Trying out squash and merge

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+import json
+import requests
+from requests.auth import HTTPBasicAuth
+
+SPOTIFY_GET_PROFILE_URL = "https://api.spotify.com/v1/me"
+# Access token expires after one hour
+ACCESS_TOKEN = "BQAqMD1uAzwcEeFtTD9TYllRYYJc9haSKiDw4H8rG8Zyc_AWu2XyPpFe2jfRr0iNz2dhGSjyy1lHa3A2sFG4rSzYvluhbjYCOOEnq6ljP4D6NJvoXquRwNMYAVxaEXgmJK512VaX9iLJN3VqsJ2ThwCtnGHY350s4q_Av-n147LjKxr0BJ20xxjsJsvg5zg"
+
+
+def get_profile():
+    response = requests.get(
+        SPOTIFY_GET_PROFILE_URL, headers={"Authorization": f"Bearer {ACCESS_TOKEN}"}
+    )
+    json_resp = response.json()
+    return json.dumps(json_resp, indent=4)
+
+
+def main():
+    profile = get_profile()
+    print(profile)
+
+
 if __name__ == "__main__":
-    ...
+    main()
