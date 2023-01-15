ab13bb97edbc634cae1149d1a41a3942ef808582
Brandon Luong <brandonbluong@gmail.com>
2023-01-15 14:36:01 -0800

    feat: create response object and add all followed artists in Brandon Luong's user profile (#7)

--- main.py
+++ main.py
@@ -1,23 +1,50 @@
 import json
 import requests
 from requests.auth import HTTPBasicAuth
+from urllib.parse import urlencode
 
-SPOTIFY_GET_PROFILE_URL = "https://api.spotify.com/v1/me"
+ENDPOINT = "https://api.spotify.com/v1/me/following"
 # Access token expires after one hour
-ACCESS_TOKEN = "BQAqMD1uAzwcEeFtTD9TYllRYYJc9haSKiDw4H8rG8Zyc_AWu2XyPpFe2jfRr0iNz2dhGSjyy1lHa3A2sFG4rSzYvluhbjYCOOEnq6ljP4D6NJvoXquRwNMYAVxaEXgmJK512VaX9iLJN3VqsJ2ThwCtnGHY350s4q_Av-n147LjKxr0BJ20xxjsJsvg5zg"
+ACCESS_TOKEN = "BQCTADw7cH-d3uOwA1efqN1grjOpjq_wJopKA7YlR4WoahoTeKsEi8qGNERH8nSpLjEDDF5YScbB6skCZWQTTP00oD_s9UpgEN033S2epawL3YXTSywZEWRdmQrclPgOzc_Y1SW7yXb7f5n34HHgBlztTsjbGfIQtZibU57s0Q760FGqE5ZSkic"
 
 
-def get_profile():
+def get_followed_artists(last_artist_id=None):
+    # create lookup_url
+    if last_artist_id:
+        query_params = {"type": "artist", "after": f"{last_artist_id}", "limit": "50"}
+    else:
+        query_params = {"type": "artist", "limit": "50"}
+
+    lookup_url = f"{ENDPOINT}?{urlencode(query_params)}"
+
+    # pass lookup_url into requests
     response = requests.get(
-        SPOTIFY_GET_PROFILE_URL, headers={"Authorization": f"Bearer {ACCESS_TOKEN}"}
+        lookup_url, headers={"Authorization": f"Bearer {ACCESS_TOKEN}"}
     )
-    json_resp = response.json()
-    return json.dumps(json_resp, indent=4)
+    return response.json()
+
+
+def add_artist_page(query, total_artists=list()):
+
+    # query['artists'].keys() -> ['items', 'next', 'total', 'cursors', 'limit', 'href']
+    page = query["artists"]["items"]  # data
+    page_artist = [artist["name"] for artist in page]
+    total_artists.extend(page_artist)
+
+    next_artist_id = query["artists"]["cursors"].get("after", None)
+
+    if next_artist_id:
+        next_query = get_followed_artists(next_artist_id)
+        add_artist_page(next_query, total_artists)
+
+    return total_artists
 
 
 def main():
-    profile = get_profile()
-    print(profile)
+
+    response = get_followed_artists()
+    total_artists = sorted(add_artist_page(response))
+    print(total_artists)
 
 
 if __name__ == "__main__":
